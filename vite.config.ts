7cb723e0657e2ac28b1aefe44f66333d1588f0a3
nathanrobinson <nathanrobinson@yahoo.com>
2025-08-22 22:44:15 -0500

    add gh-pages action

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TornRankedWarReport/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
